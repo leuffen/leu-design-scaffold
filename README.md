c931c2e51a015818d5f098877ae64be55bcecc18
Matthias Leuffen <matthes@leuffen.de>
2023-08-28 12:35:16 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # leu-design-scaffold
+
+## Auschecken und Loslegen
+
+- Folgendes muss installiert sein: NodeJS mit NPM
+- Zum Starten: npm update && npm run dev
+
+
+## Starten und Entwickeln
+
+Im Projektverzeichnis
+```
+webpack-dev-server
+```
+
+ausführen. Die Website aus `html/index.html` ist dann unter http://localhost:4000 erreichbar.
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        'assets/style': './src/_style.scss',
+        'assets/bs': './src/_bs.scss',
+
+    },
+    cache: true,
+    module: {
+        rules: [
+
+            {
+                enforce: 'pre',
+                test: /\.html$/,
+                loader: 'raw-loader',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+
+                        loader: 'css-loader',
+                        options: {
+                            url: false // don't complain about url() in css
+                        }
+
+                    }, 'sass-loader'],
+                include: path.resolve(__dirname, "")
+            },
+
+
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    //devtool: 'source-map',
+    watch: true,
+    mode: "development",
+    devServer: {
+        client: {
+            overlay: true,
+        },
+        static: {
+            directory: path.join(__dirname, 'html'),
+            watch: true,
+        },
+        liveReload: true,
+        watchFiles: {
+            paths: ['src/*', 'html/*']
+        },
+        port: 4000,
+        hot: false
+    },
+    plugins: [
+
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: "[name].css",
+          chunkFilename: "[id].css",
+        }),
+
+
+    ],
+
+
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '/tmp/'),
+    },
+};
